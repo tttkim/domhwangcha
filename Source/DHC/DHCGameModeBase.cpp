678b62dc203f7e3feb562d36a8776edee7ab4da4
taeha <rlaxogk006@naver.com>
2021-07-16 11:34:31 +0900

    modified GameModeBase

--- Source/DHC/DHCGameModeBase.cpp
+++ Source/DHC/DHCGameModeBase.cpp
@@ -2,16 +2,10 @@
 
 
 #include "DHCGameModeBase.h"
-#include "DHCPawn.h"
 #include "DHCPlayerController.h"
 
 ADHCGameModeBase::ADHCGameModeBase()
 {
-	DefaultPawnClass = ADHCPawn::StaticClass();
 	PlayerControllerClass = ADHCPlayerController::StaticClass();
-	static ConstructorHelpers::FClassFinder<APawn> BP_PAWN_C(TEXT("/Game/ThirdPersonBP/Blueprints/ThirdPersonCharacter.ThirdPersonCharacter_C"));
-	if (BP_PAWN_C.Succeeded())
-	{
-		DefaultPawnClass = BP_PAWN_C.Class;
-	}
+
 }
